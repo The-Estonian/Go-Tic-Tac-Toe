536e8923c362b77be712a4ae833319429dbb8897
The-Estonian <Zaar2213@mail.com>
2023-05-27 22:26:39 +0300

    Created visual grid and X+O for the game

--- grid/main.go
+++ grid/main.go
@@ -4,6 +4,46 @@ import (
 	"fmt"
 )
 
+var a = ("┌──────────┬──────────┬──────────┐")
+var b = ("│          │          │          │")
+var c = ("│          │          │          │")
+var d = ("│          │          │          │")
+var e = ("│          │          │          │")
+var f = ("├──────────┼──────────┼──────────┤")
+var g = ("│          │          │          │")
+var h = ("│          │          │          │")
+var i = ("│          │          │          │")
+var j = ("│          │          │          │")
+var k = ("├──────────┼──────────┼──────────┤")
+var l = ("│          │          │          │")
+var m = ("│          │          │          │")
+var n = ("│          │          │          │")
+var o = ("│          │          │          │")
+var p = ("└──────────┴──────────┴──────────┘")
+
+var a1 = ("╲  ╱")
+var a2 = (" ╲╱ ")
+var a3 = (" ╱╲ ")
+var a4 = ("╱  ╲")
+
+var b1 = ("┌─────┐")
+var b2 = ("│     │")
+var b3 = ("│     │")
+var b4 = ("└─────┘")
+
 func FullGrid() {
 	fmt.Println("Grid package Full Grid func")
+	gridList := []string{a, b, c, d, e, f, g, h, i, j, k, l, m, n, o, p}
+	for i := 0; i < len(gridList); i++ {
+		fmt.Println(gridList[i])
+	}
+	fmt.Println(a1)
+	fmt.Println(a2)
+	fmt.Println(a3)
+	fmt.Println(a4)
+
+	fmt.Println(b1)
+	fmt.Println(b2)
+	fmt.Println(b3)
+	fmt.Println(b4)
 }
