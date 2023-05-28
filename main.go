cbdec20d55df3560c0f8955ef338534ca1e3b114
The-Estonian <Zaar2213@mail.com>
2023-05-28 15:39:50 +0300

    Added logic to test visual logic for grid

--- main.go
+++ main.go
@@ -8,7 +8,7 @@ import (
 func gameLogic() {
 	game := true
 	for game {
-		grid.FullGrid()
+		grid.PrintGrid()
 		userInput := ""
 		fmt.Print(`exit - to exit
 o to enter o
@@ -18,14 +18,32 @@ x to enter x
 		if userInput == "exit" {
 			game = false
 		} else if userInput == "1" {
-			grid.PlacePlayer("x", 2)
-			grid.FullGrid()
-		} else if userInput == "2" {
 			grid.PlacePlayer("x", 1)
-			grid.FullGrid()
+			grid.PrintGrid()
+		} else if userInput == "2" {
+			grid.PlacePlayer("o", 2)
+			grid.PrintGrid()
 		} else if userInput == "3" {
 			grid.PlacePlayer("x", 3)
-			grid.FullGrid()
+			grid.PrintGrid()
+		} else if userInput == "4" {
+			grid.PlacePlayer("o", 4)
+			grid.PrintGrid()
+		} else if userInput == "5" {
+			grid.PlacePlayer("x", 5)
+			grid.PrintGrid()
+		} else if userInput == "6" {
+			grid.PlacePlayer("o", 6)
+			grid.PrintGrid()
+		} else if userInput == "7" {
+			grid.PlacePlayer("x", 7)
+			grid.PrintGrid()
+		} else if userInput == "8" {
+			grid.PlacePlayer("o", 8)
+			grid.PrintGrid()
+		} else if userInput == "9" {
+			grid.PlacePlayer("x", 9)
+			grid.PrintGrid()
 		}
 	}
 }
