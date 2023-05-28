22246c1d6da564f4801deb27ecd5b4edac8db8b6
The-Estonian <Zaar2213@mail.com>
2023-05-28 21:29:56 +0300

    Created basic loading screen for eye candy

---
+++ loadingScreen/main.go
@@ -0,0 +1,26 @@
+package loadingScreen
+
+import (
+	"fmt"
+	"math/rand"
+	"time"
+	"01.kood.tech/git/jsaar/tic_tac_toe/screenClear"
+)
+
+var loadingVisuals map[int]string = map[int]string{
+	1: "┌────────────────────┐",
+	2: "│                    │",
+	3: "└────────────────────┘",
+	4: "|",
+}
+
+func Load() {
+	for i := 1; i < 21; i++ {
+		screenClear.Now()
+		loadingVisuals[2] = loadingVisuals[2][:i+2] + loadingVisuals[4] + loadingVisuals[2][i+3:]
+		fmt.Println(loadingVisuals[1])
+		fmt.Println(loadingVisuals[2])
+		fmt.Println(loadingVisuals[3])
+		time.Sleep(time.Duration(rand.Intn(150)* int(time.Millisecond)))
+	}
+}
